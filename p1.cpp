cd455ed9e447ef6ca079f5370eb0a52ab0e3add7
PasyaMaulana <pasyamaulana17@gmail.com>
2025-03-10 11:39:51 +0700

    prosedur

---
+++ p1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+float luas, panjang, lebar;
+
+void inputData()
+{
+    cout << "Masukkan panjangnya:";
+    cin >> panjang;
+
+    cout << "Masukkan lebarnya:";
+    cin >> lebar;
+}
+
+void prosedurLuas()
+{
+    luas = panjang * lebar;
+}
+
+void tampilkanLuas()
+{
+    cout << "Luas persegi panjang:" << luas << endl;
+}
+
+int main()
+{
+    inputData();
+    prosedurLuas();
+    tampilkanLuas();
+}
